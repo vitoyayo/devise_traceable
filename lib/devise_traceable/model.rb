d27626d144a2fed7b5d4542eb4793fb5bf5cfadf
sukhchander <sukhchander@gmail.com>
2013-10-18 11:43:09 -0400

    log user.current_sign_in_ip

--- lib/devise_traceable/model.rb
+++ lib/devise_traceable/model.rb
@@ -2,16 +2,16 @@ require 'devise_traceable/hooks/traceable'
 
 module Devise
   module Models
-    # Trace information about your user sign in. It tracks the following columns:
-
-    # * resource_id
-    # * sign_in_at
-    # * sign_out_at
-    
     module Traceable
       def stamp!
         new_current = Time.now
-        "#{self.class}Tracing".constantize.create(:sign_in_at => self.current_sign_in_at, :sign_out_at => new_current, "#{self.class}".foreign_key.to_sym => self.id)
+
+        "#{self.class}Tracing".constantize.create(
+          ip: self.current_sign_in_ip,
+          sign_in_at: self.current_sign_in_at,
+          sign_out_at: new_current,
+          "#{self.class}".foreign_key.to_sym => self.id
+        )
       end
     end
   end
