66a372316e09a3ee16b26852e4f753d42c626ad5
Victor Cedeno <vicedeno@gmail.com>
2021-11-08 17:06:11 -0300

    add support rails 6 migrations template

--- lib/devise_traceable/model.rb
+++ lib/devise_traceable/model.rb
@@ -11,8 +11,6 @@ module Devise
           sign_out_at: self.Time.now,
           sign_out_ip: self.current_location.to_s,
           user_agent: self.user_agent.to_s,
-          device_type: self.device_type.to_s,
-          device_name: self.device_name.to_s,
           "#{self.class}".foreign_key.to_sym => self.id
         )
       end
--- lib/generators/devise_traceable/templates/migration.rb
+++ lib/generators/devise_traceable/templates/migration.rb
@@ -1,21 +1,19 @@
-class DeviseCreateUserTracings < ActiveRecord::Migration
+class DeviseCreateUserTracings < ActiveRecord::Migration[6.1]
   def self.up
-    create_table :<%= table_name.singularize %>_tracings do |t|
-      t.integer  :<%= table_name.classify.foreign_key  %>
+    create_table :user_tracings do |t|
+      t.references :user, foreign_key: true, type: :uuid
       t.datetime :sign_in_at
       t.datetime :sign_out_at
       t.string :sign_in_ip
       t.string :sign_out_ip
       t.string :user_agent
-      t.string :device_type
-      t.string :device_name
       t.timestamps
   end
 
-  add_index :<%= table_name.singularize %>_tracings, :<%= table_name.classify.foreign_key  %>
+  add_index  :"#{table_name.singularize}_tracings", :"#{table_name.classify.foreign_key}"
 end
 
 def self.down
-  drop_table :<%= table_name.singularize %>_tracings
+  drop_table : "#{table_name.singularize}_tracings"
 end
 end
